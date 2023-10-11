159e2eeec570b54098b2d7f955ddc7a6e148c4f8
kanghee96 <kanghee96@gmail.com>
2023-10-11 12:55:16 +0900

    :heavy_plus_sign: feat: spring 기본 설정 구현

---
+++ backend/A201/src/main/java/com/example/A201/A201Application.java
@@ -0,0 +1,13 @@
+package com.example.A201;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class A201Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(A201Application.class, args);
+	}
+
+}
