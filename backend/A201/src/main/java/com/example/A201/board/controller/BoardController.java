0bd06071c3bc5ef9f33fd4dce8961c741eb7e153
ji <6531z@naver.com>
2023-10-14 01:46:41 +0900

    ➕ Feat(feture/#5): board api 데이타 controller 개발 및 exception 핸들러 및 커스텀 에러 추가

--- backend/A201/src/main/java/com/example/A201/board/controller/BoardController.java
+++ backend/A201/src/main/java/com/example/A201/board/controller/BoardController.java
@@ -1,6 +1,8 @@
 package com.example.A201.board.controller;
 
 import com.example.A201.board.service.MetadataService;
+import com.example.A201.board.service.TestdataService;
+import com.example.A201.board.vo.TestdataResponse;
 import com.example.A201.exception.SuccessResponseEntity;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -9,15 +11,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequiredArgsConstructor
-@RequestMapping("/api/board")
-import com.example.A201.board.service.TestdataService;
-import com.example.A201.board.vo.TestdataResponse;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
 @RestController
@@ -25,6 +18,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class BoardController {
     private final MetadataService metadataService;
+    private final TestdataService testdataService;
 
     @GetMapping("/metadata/{code}/{type}")
     public ResponseEntity<SuccessResponseEntity> getMetadataType(@PathVariable("code") String code,
@@ -37,9 +31,8 @@ public class BoardController {
         return SuccessResponseEntity.toResponseEntity("베터리 코드와 타입별 데이터 불러오기 성공", metadataService.getMetadataCode(code));
     }
 
-    private final TestdataService testdataService;
     @GetMapping("/{uid}/testdatas")
-    public ResponseEntity<?> readTestdataList(@PathVariable("uid") long uid){
+    public ResponseEntity<?> readTestdataList(@PathVariable("uid") long uid) {
         List<TestdataResponse> responses = testdataService.readTestdataList(uid);
         return ResponseEntity.ok().body(responses);
     }
