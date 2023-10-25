147213fe0ed6f3ee575d3ce975c236c479b98190
jongseong <whd1707@gmail.com>
2023-10-25 12:41:14 +0900

    ➕ Feat(feature/#14): Battery, Model  수정

--- backend/A201/src/main/java/com/example/A201/battery/domain/Battery.java
+++ backend/A201/src/main/java/com/example/A201/battery/domain/Battery.java
@@ -16,27 +16,16 @@ public class Battery {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "battery_id")
     private Long id;
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
     private Member memberId;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "model_id")
+    private Model model;
+
     private String code;
-
-    private Double overVoltage;   // 과전압 임계값
-
-    private Double underVoltage;  // 저전압 임계값
-
-    private Double chargingOverCurrent; // 충전시 과전류 임계값
-
-    private Double chargingMaxTemperature; // 충전시 최고 온도 임계값
-
-    private Double chargingMinTemperature;  // 충전시 최저 온도 임계값
-
-    private Double dischargingOverCurrent;  // 방전시 과전류 임계값
-
-    private Double dischargingMaxTemperature;  // 방전시 최고 온도 임계값
-
-    private Double dischargingMinTemperature;  // 방전시 최저 온도 임계값
 
     private LocalDate madeDate;
 
