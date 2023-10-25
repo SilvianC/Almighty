defdd3f272f505d12316fde2ca458788e4a4187d
jongseong <whd1707@gmail.com>
2023-10-25 14:01:42 +0900

    ➕ Feat(feature/#14): 구매내역 api 개발

--- backend/A201/src/main/java/com/example/A201/battery/service/BatteryService.java
+++ backend/A201/src/main/java/com/example/A201/battery/service/BatteryService.java
@@ -1,12 +1,14 @@
 package com.example.A201.battery.service;
 
-import com.example.A201.battery.vo.BatteryCodeResponse;
+import com.example.A201.battery.vo.BatteryResponse;
 import com.example.A201.battery.vo.BatterydataResponse;
 
 import java.util.List;
 
 public interface BatteryService {
-    List<BatteryCodeResponse> getBatteries();
+    List<BatteryResponse> getBatteriesAll();
 
     BatterydataResponse getBattery(String code);
+
+    List<BatteryResponse> getBatteries(Long memberId);
 }
--- backend/A201/src/main/java/com/example/A201/battery/service/BatteryServiceImpl.java
+++ backend/A201/src/main/java/com/example/A201/battery/service/BatteryServiceImpl.java
@@ -2,7 +2,7 @@ package com.example.A201.battery.service;
 
 import com.example.A201.battery.domain.Battery;
 import com.example.A201.battery.repository.BatteryRepository;
-import com.example.A201.battery.vo.BatteryCodeResponse;
+import com.example.A201.battery.vo.BatteryResponse;
 import com.example.A201.battery.vo.BatterydataResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -18,13 +18,19 @@ public class BatteryServiceImpl implements BatteryService{
 
     private final BatteryRepository batteryRepository;
     @Override
-    public List<BatteryCodeResponse> getBatteries() {
+    public List<BatteryResponse> getBatteriesAll() {
         List<Battery> batteries = batteryRepository.findAll();
-        return batteries.stream().map(battery -> BatteryCodeResponse.batteryCodeResponse(battery)).collect(Collectors.toList());
+        return batteries.stream().map(battery -> BatteryResponse.batteryCodeResponse(battery)).collect(Collectors.toList());
     }
     @Override
     public BatterydataResponse getBattery(String code){
         return batteryRepository.findByCode(code).map(battery -> BatterydataResponse.batteryResponse(battery))
                 .orElseThrow(() -> new IllegalStateException("해당 배터리를 찾을 수 없습니다"));
+    }
+
+    @Override
+    public List<BatteryResponse> getBatteries(Long memberId){
+        List<Battery> batteries = batteryRepository.findByMember(memberId);
+        return batteries.stream().map(battery -> BatteryResponse.batteryCodeResponse(battery)).collect(Collectors.toList());
     }
 }
