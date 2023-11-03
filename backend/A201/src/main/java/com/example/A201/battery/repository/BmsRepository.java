fd98ed0e6c7640a5baacd13614f9dd4b19789241
조태규 <hey03312@gmail.com>
2023-11-03 17:19:24 +0900

    ➕ Feat(feature/#5) : BMS Fault 도메인 추가

--- backend/A201/src/main/java/com/example/A201/battery/repository/BmsRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.A201.battery.repository;
-
-import com.example.A201.battery.domain.Bms;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BmsRepository  extends JpaRepository<Bms,Long> {
-}
