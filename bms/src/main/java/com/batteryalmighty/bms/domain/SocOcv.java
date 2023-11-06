5f9a899064b4be5fa0a04583d79e72628b891a01
jongseong <whd1707@gmail.com>
2023-11-06 10:39:59 +0900

    ➕ Feat(feature/#33): BMS 시스템 개발

---
+++ bms/src/main/java/com/batteryalmighty/bms/domain/SocOcv.java
@@ -0,0 +1,16 @@
+package com.batteryalmighty.bms.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class SocOcv {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "soc_ocv_id")
+    private Long id;
+}
