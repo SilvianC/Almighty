56b3a32fa7abcf4913ec62c24a9d7e922e7dbe10
kanghee96 <kanghee96@gmail.com>
2023-11-09 15:24:14 +0900

    :heavy_plus_sign: feat(feature/#11): gpt 분석 구현

--- frontend/blog/src/components/chatbot/ChatComponent.jsx
+++ frontend/blog/src/components/chatbot/ChatComponent.jsx
@@ -1,50 +1,75 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useRecoilValue } from "recoil";
 import { MemberIdState } from "../../states/states";
 import http from '../../api/http';
 import styled from "styled-components";
+
 const ChatComponent = () => {
-    const [message, setMessage] = useState(''); // 사용자가 입력하는 메시지
-    const [chatLog, setChatLog] = useState([]); // 채팅 로그
+    const [botResponse, setBotResponse] = useState('');
     const memberId = useRecoilValue(MemberIdState);
 
-    const sendMessage = async () => {
+    useEffect(() => {
+        getBotResponse();
+    }, []); // 빈 의존성 배열을 사용하여 컴포넌트 마운트 시 한 번만 실행됩니다.
+
+    const getBotResponse = async () => {
         try {
-            const response = await http.post('/api/chat/interact', {
-                userMessage: message,
-                memberId : memberId
+            // progressId는 상황에 맞게 설정해야 합니다.
+            const progressId = 6; 
+            const response = await http.post(`/api/chat/interact/${progressId}`, {
+                timestamp: new Date(), // 현재 시간을 사용
+                memberId: memberId,
+                progressId: progressId
             });
-            setChatLog([...chatLog, { user: 'You', message: message }, { user: 'Bot', message: response.data.botResponse }]);
-            setMessage('');
+            setBotResponse(response.data.botResponse);
         } catch (error) {
-            console.error('Error sending message:', error);
+            console.error('Error getting response:', error);
         }
+    };
+    // 개행 문자를 <br />로 변환하고 단어에 따라 색상을 지정
+    const formatResponse = (response) => {
+        const responseLines = response.split('\n');
+        let backgroundColor = 'white'; // 기본 배경색
+        let color = 'black'; // 기본 색상
+        if (responseLines[0].includes('불량')) {
+            backgroundColor = 'pink'; // '불량'일 경우 배경색
+            color = 'red';
+        } else if (responseLines[0].includes('정상')) {
+            backgroundColor = 'lightblue'; // '정상'일 경우 배경색
+            color = 'blue';
+        }
+
+        const formattedResponse = responseLines.map((line, index) => (
+            <span key={index}>{line}<br /></span>
+        ));
+
+        return <ResponseWrapper backgroundColor={backgroundColor} style={{ color: color }}>{formattedResponse}</ResponseWrapper>;
     };
 
     return (
         <S.MainContent>
-        <div>
-            <div className="chat-box">
-                {chatLog.map((entry, index) => (
-                    <div key={index}>
-                        <strong>{entry.user}:</strong> {entry.message}
+            <div>
+                {botResponse && (
+                    <div>
+                        <strong>Bot Response:</strong> {formatResponse(botResponse)}
                     </div>
-                ))}
+                )}
             </div>
-            <input
-                type="text"
-                value={message}
-                onChange={e => setMessage(e.target.value)}
-            />
-            <button onClick={sendMessage}>Send</button>
-        </div>
         </S.MainContent>
     );
 };
+
 const S = {
     MainContent: styled.div`
-      padding-top: 60px; // 상단 navbar의 높이만큼 패딩을 줍니다.
-      padding-left: 100px; // 왼쪽 navbar의 너비만큼 패딩을 줍니다.
+        padding-top: 60px; // 상단 navbar의 높이만큼 패딩을 줍니다.
+        padding-left: 100px; // 왼쪽 navbar의 너비만큼 패딩을 줍니다.
     `,
-  };
+};
+// 새로운 styled 컴포넌트로 ResponseWrapper 정의
+const ResponseWrapper = styled.div`
+    background-color: ${props => props.backgroundColor};
+    padding: 10px;
+    margin: 10px 0;
+    border-radius: 8px;
+`;
 export default ChatComponent;
