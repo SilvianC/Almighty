a16065d658acd68395f459429e1449aa9531d8c4
jongseong <whd1707@gmail.com>
2023-11-10 15:26:47 +0900

    ➕ Feat(feature/#2): 그래프 클릭 이벤트

--- frontend/blog/src/components/graph/BmsGraph.jsx
+++ frontend/blog/src/components/graph/BmsGraph.jsx
@@ -4,8 +4,7 @@ import HighchartsReact from "highcharts-react-official";
 import styled from "styled-components";
 
 const BmsGraph = ({ data, threshold, type, num }) => {
-  console.log(data);
-  const option = {
+  let option = {
     chart: {
       type: "column",
     },
@@ -29,86 +28,90 @@ const BmsGraph = ({ data, threshold, type, num }) => {
       text: "",
       align: "left",
     },
-
-    yAxis: {
-      title: {
-        text: "Count",
-      },
-    },
-
-    xAxis: { type: "category" },
-
-    legend: {
-      enabled: false, // 범례 비활성화
-    },
-
-    plotOptions: {
-      label: {
-        connectorAllowed: false,
-      },
-      line: {
-        // 선 그래프에 대한 설정
-        lineWidth: 2, // 선의 굵기 설정 (기본값은 2)
-      },
-    },
-
-    series: [
-      {
-        colors: [
-          "#9b20d9",
-          "#9215ac",
-          "#861ec9",
-          "#7a17e6",
-          "#7010f9",
-          "#691af3",
-          "#6225ed",
-          "#5b30e7",
-          "#533be1",
-          "#4c46db",
-          "#4551d5",
-          "#3e5ccf",
-          "#3667c9",
-          "#2f72c3",
-          "#277dbd",
-          "#1f88b7",
-          "#1693b1",
-          "#0a9eaa",
-          "#03c69b",
-          "#00f194",
-        ],
-        colorByPoint: true,
-
-        data: [
-          ["과전압", data["overVoltageCount"]],
-          ["저전압", data["underVoltageCount"]],
-          ["과전류", data["overCurrentCount"]],
-          ["온도이상", data["abnormalTemperatureCount"]],
-        ],
-        dataLabels: {
-          enabled: true,
-          color: "#FFFFFF",
-          align: "right",
-          format: "{point.y:.f}", // one decimal
-          y: 10, // 10 pixels down from the top
-          style: {
-            fontSize: "13px",
-            fontFamily: "Verdana, sans-serif",
-          },
+  };
+  if (data) {
+    option = {
+      ...option,
+      yAxis: {
+        title: {
+          text: "Count",
         },
       },
-    ],
 
-    responsive: {
-      rules: [
+      xAxis: { type: "category" },
+
+      legend: {
+        enabled: false, // 범례 비활성화
+      },
+
+      plotOptions: {
+        label: {
+          connectorAllowed: false,
+        },
+        line: {
+          // 선 그래프에 대한 설정
+          lineWidth: 2, // 선의 굵기 설정 (기본값은 2)
+        },
+      },
+
+      series: [
         {
-          condition: {
-            maxWidth: 500,
+          colors: [
+            "#9b20d9",
+            "#9215ac",
+            "#861ec9",
+            "#7a17e6",
+            "#7010f9",
+            "#691af3",
+            "#6225ed",
+            "#5b30e7",
+            "#533be1",
+            "#4c46db",
+            "#4551d5",
+            "#3e5ccf",
+            "#3667c9",
+            "#2f72c3",
+            "#277dbd",
+            "#1f88b7",
+            "#1693b1",
+            "#0a9eaa",
+            "#03c69b",
+            "#00f194",
+          ],
+          colorByPoint: true,
+
+          data: [
+            ["과전압", data["overVoltageCount"]],
+            ["저전압", data["underVoltageCount"]],
+            ["과전류", data["overCurrentCount"]],
+            ["온도이상", data["abnormalTemperatureCount"]],
+          ],
+          dataLabels: {
+            enabled: true,
+            color: "#FFFFFF",
+            align: "right",
+            format: "{point.y:.f}", // one decimal
+            y: 10, // 10 pixels down from the top
+            style: {
+              fontSize: "13px",
+              fontFamily: "Verdana, sans-serif",
+            },
           },
-          chartOptions: {},
         },
       ],
-    },
-  };
+
+      responsive: {
+        rules: [
+          {
+            condition: {
+              maxWidth: 500,
+            },
+            chartOptions: {},
+          },
+        ],
+      },
+    };
+  }
 
   return (
     <S.Wrap>
