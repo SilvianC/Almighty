602d6ba89b66ddbd6450f169a20b84aef8f8efba
6531z <6531z@naver.com>
2023-11-14 17:11:19 +0900

    📝 Docs(feature/#38) : .env 수정

--- frontend/blog/src/config/firebase-messaging-sw.js
+++ frontend/blog/src/config/firebase-messaging-sw.js
@@ -12,9 +12,9 @@ function FirebaseComponent() {
   const [isLogin, setIsLogin] = useRecoilState(IsLoginState);
   const firebaseConfig = {
     apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    authDomain: "a201-822f6.firebaseapp.com",
+    projectId: "a201-822f6",
+    storageBucket: "a201-822f6.appspot.com",
     messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
     appId: process.env.REACT_APP_APP_ID,
     measurementId: process.env.REACT_APP_MEASUREMENT_ID,
