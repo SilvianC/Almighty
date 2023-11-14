5a7cea130c3de1879806451326536f0fb745e7dc
6531z <6531z@naver.com>
2023-11-14 16:53:38 +0900

    📝 Docs(feature/#38) : fcm config 수정

--- frontend/blog/src/config/firebase-messaging-sw.js
+++ frontend/blog/src/config/firebase-messaging-sw.js
@@ -11,20 +11,19 @@ function FirebaseComponent() {
   const memberId = useRecoilValue(MemberIdState);
   const [isLogin, setIsLogin] = useRecoilState(IsLoginState);
   const firebaseConfig = {
-    apiKey: "AIzaSyDmHguVkQXMt9KJyp26qRwA25oocAs7L50",
-    authDomain: "a201-822f6.firebaseapp.com",
-    projectId: "a201-822f6",
-    storageBucket: "a201-822f6.appspot.com",
-    messagingSenderId: "274097286993",
-    appId: "1:274097286993:web:3480ae4ea661778fd7ad70",
-    measurementId: "G-TP3XY36SH9",
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
+    measurementId: process.env.REACT_APP_MEASUREMENT_ID,
   };
 
   const app = initializeApp(firebaseConfig);
   const messaging = getMessaging(app);
 
   useEffect(() => {
-    console.log(isLogin);
     if (isLogin == "") {
       requestPermission();
     }
